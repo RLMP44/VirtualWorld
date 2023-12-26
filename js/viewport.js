6cc74204746d5602482b9cee73ac947e69f57e14
Rachael Momber <rlmomber@gmail.com>
2023-12-26 20:01:14 +0900

    add mousewheel event listener

--- js/viewport.js
+++ js/viewport.js
@@ -1,6 +1,22 @@
 class Viewport {
   constructor(canvas) {
     this.canvas = canvas;
+    // get context via the canvas once more
     this.ctx = canvas.getContext("2d");
+
+    // set to default
+    this.zoom = 1;
+
+    // to change the zoom
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    // listening to mouse wheel
+    this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+  }
+
+  #handleMouseWheel(evt) {
+    console.log(evt.deltaY);
   }
 }
