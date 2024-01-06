9254fe973f7779d957be792df7fe1c6d2fa4ee09
Rachael Momber <rlmomber@gmail.com>
2024-01-06 12:16:18 +0900

    added border radius to end of polygon envelope, got rid of old points that were hardcoded, get points of segment and connect them, adding border radius to the ends of both

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -16,12 +16,24 @@ class Envelope {
     const alpha_ccw = alpha - Math.PI / 2;
     // determines the offset we move it along the angle
     // creates our 4 points along the segment
-    const point1_ccw = translate(point1, alpha_ccw, radius);
-    const point2_ccw = translate(point2, alpha_ccw, radius);
-    const point2_cw = translate(point2, alpha_cw, radius);
-    const point1_cw = translate(point1, alpha_cw, radius);
+    // const point1_ccw = translate(point1, alpha_ccw, radius);
+    // const point2_ccw = translate(point2, alpha_ccw, radius);
+    // const point2_cw = translate(point2, alpha_cw, radius);
+    // const point1_cw = translate(point1, alpha_cw, radius);
 
-    return new Polygon([point1_ccw, point2_ccw, point2_cw, point1_cw]);
+    // adds border radius manually
+    const step = Math.PI / 10;
+    const points = [];
+    // add points to be used to make polygon
+    for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+      points.push(translate(point1, i, radius));
+    }
+    // handles the second point but turns 180 degrees so rounded edge is on the outside
+    for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+      points.push(translate(point2, Math.PI + i, radius));
+    }
+
+    return new Polygon(points);
   }
 
   draw(ctx) {
