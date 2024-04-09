d40b182a68a1165e7e372522218b2aa5ab6e8269
Rachael Momber <rlmomber@gmail.com>
2024-04-09 19:38:32 +0900

    add: create guides in generate buildings method to determine if buildings can fit

--- js/world.js
+++ js/world.js
@@ -48,7 +48,10 @@ class World {
         )
       );
     }
-    return tmpEnvelopes;
+
+    const guides = Polygon.union(tmpEnvelopes.map((e) => e.poly));
+
+    return guides;
   }
 
   draw(ctx) {
